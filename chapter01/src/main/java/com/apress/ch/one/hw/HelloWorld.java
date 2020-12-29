889b70b83330518c37d023fcfed5f17facd0aa3f
iuliana <iuliana.cosmina@gmail.com>
2020-12-29 15:10:04 +0000

    started writing code and added support for modules

--- chapter01/src/main/java/com/apress/ch/one/hw/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.apress.ch.one.hw;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- chapter01/src/main/java/module-info.java
+++ chapter01/src/main/java/module-info.java
@@ -1,13 +1,19 @@
+<<<<<<< HEAD
 import com.apress.bgn.one.service.Provider;
 import com.apress.bgn.zero.service.NakedService;
 
+=======
+>>>>>>> started writing code and added support for modules
 /**
  * Created by iuliana.cosmina on 1/14/18.
  */
 module chapter.one {
     requires chapter.zero;
 
+<<<<<<< HEAD
     // needed for Appendix A.
     exports com.apress.bgn.one.service;
     provides NakedService with Provider;
+=======
+>>>>>>> started writing code and added support for modules
 }
