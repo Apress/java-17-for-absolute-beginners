61b192bc537c78186eac2260bf544fa388cfb20d
iuliana <iuliana.cosmina@gmail.com>
2021-02-04 00:59:56 +0000

    added more module configurations

--- chapter03/src/main/java/module-info.java
+++ chapter03/src/main/java/module-info.java
@@ -10,12 +10,18 @@ module chapter.three {
     requires chapter.zero;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     // not really needed
     //requires java.base;
 
     // needed for Appendix A.
     uses NakedService;
 =======
+=======
+    // not really needed
+    //requires java.base;
+
+>>>>>>> added more module configurations
     //uses com.apress.bgn.zero.service.NakedService;
 >>>>>>> continue work on chapter 03
 }
