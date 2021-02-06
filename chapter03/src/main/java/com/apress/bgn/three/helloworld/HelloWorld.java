f77fc22ab6fd53aee77e87018104b82083a8fc67
iuliana <iuliana.cosmina@gmail.com>
2021-02-06 02:24:33 +0000

    008 finished chapter 3

--- chapter03/src/main/java/com/apress/bgn/three/helloworld/HelloWorld.java
+++ chapter03/src/main/java/com/apress/bgn/three/helloworld/HelloWorld.java
@@ -8,5 +8,13 @@ package com.apress.bgn.three.helloworld;
 public class HelloWorld {
     public static void main(String... args) {
         System.out.println("Hello " + args[0] + "!");
+<<<<<<< HEAD
+=======
+
+     /*
+      List<String> items = List.of("1", "a", "2", "a", "3", "a");
+      items.forEach(item -> System.out.println(item.equals("a") ? "A" : "Not A"));
+     */
+>>>>>>> 008 finished chapter 3
     }
 }
