c5f22e92919b65423e1c7a161efe253dd2e50645
iuliana <iuliana.cosmina@gmail.com>
2021-09-23 17:50:26 +0100

    Added circleci config file

--- chapter01/src/main/java/com/apress/bgn/one/Example01.java
+++ chapter01/src/main/java/com/apress/bgn/one/Example01.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Example01 {
     public static void main(String[] args) {
+
         List<String> items = List.of("1", "a", "2", "a", "3", "a");
 
         items.forEach(item -> {
